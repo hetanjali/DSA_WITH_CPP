38558e0fb35c227228d2cc11ad538f86ef541a5d
Hetanjali Vaghela <hetanjalivaghela43@gmail.com>
2026-06-18 15:26:40 +0530

    Implement input/output functionality in Solution class

---
+++ 01_Basics/01_input_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() {
+        int x;
+        cin >> x;
+        cout << x;
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.printNumber();
+    return 0;
+}
